bcd70d032210180d8a72932e515f8d71d6b7d902
oooooa <rongming0@163.com>
2013-12-04 15:06:05 +0800

    test for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 XWeibo::Application.routes.draw do
   root "static_pages#home"
   get "static_pages/help"
+  get "static_pages/home"
+  get "static_pages/about"
   resources :microposts
 
   resources :users
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "Statici pages" do
+  describe "Home page" do
+    it "should have the content 'XWeibo'" do
+      visit '/static_pages/home'
+      expect(page).to have_content 'XWeibo'
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title "XWeibo | Home"
+    end
+  end
+
+  describe "Help page" do
+    it "should have content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content 'Help'
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title "XWeibo | Help"
+    end
+  end
+
+  describe "About page" do
+    it "should have content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content 'About'
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title "XWeibo | About"
+    end
+  end
+end
