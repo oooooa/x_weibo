4bba042714d6b8f5f176a0e1fa2429da4a73bcac
oooooa <rongming0@163.com>
2013-12-04 09:09:30 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XWeibo::Application.config.secret_key_base = '82e354ab089a518dafe05fef5d5b21823d119ece8abbc00b4d3e10d92e99bb0155211f33eebfbee8d321d8bfbb479abb830fc836f6d06dae8eb94d0b410982ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XWeibo::Application.config.session_store :cookie_store, key: '_x_weibo_session'
